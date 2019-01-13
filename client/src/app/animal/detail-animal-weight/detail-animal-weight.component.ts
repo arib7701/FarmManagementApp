4c58ca2f9b09b24cdaefec1306c586de2f4ef26a
arib7701 <amandineribot01@gmail.com>
2019-01-13 22:13:52 +0100

    add view animal info + weight info

--- client/src/app/animal/detail-animal-weight/detail-animal-weight.component.ts
+++ client/src/app/animal/detail-animal-weight/detail-animal-weight.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Weight } from 'src/app/models/weight';
+import { WeightService } from 'src/app/services/weight.service';
 
 @Component({
   selector: 'app-detail-animal-weight',
   templateUrl: './detail-animal-weight.component.html',
   styleUrls: ['./detail-animal-weight.component.css']
 })
-export class DetailAnimalWeightComponent implements OnInit {
+export class DetailAnimalWeightComponent implements OnInit, OnDestroy {
+  @Input()
+  idAnimal: number;
+  subscriptionWeights: Subscription;
+  weights: Weight[];
+  diffs = new Array<String>();
 
-  constructor() { }
+  constructor(private weightService: WeightService) {}
 
   ngOnInit() {
+    this.subscriptionWeights = this.weightService
+      .getAllWeightByAnimalId(this.idAnimal)
+      .subscribe(
+        weights => {
+          this.weights = weights;
+          for (let i = 0; i < this.weights.length; i++) {
+            this.getArrow(i);
+          }
+        },
+        error => {
+          console.log('Error getting weights');
+        }
+      );
   }
 
+  getArrow(index: number) {
+    if (index === 0) {
+      this.diffs[index] = 'start';
+    } else {
+      if (this.weights[index].measure > this.weights[index - 1].measure) {
+        this.diffs[index] = 'up';
+      } else if (
+        this.weights[index].measure === this.weights[index - 1].measure
+      ) {
+        this.diffs[index] = 'equal';
+      } else {
+        this.diffs[index] = 'down';
+      }
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.subscriptionWeights !== undefined) {
+      this.subscriptionWeights.unsubscribe();
+    }
+  }
 }
--- client/src/app/animal/detail-animal/detail-animal.component.ts
+++ client/src/app/animal/detail-animal/detail-animal.component.ts
@@ -1,15 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Animal } from 'src/app/models/animal';
+import { Subscription } from 'rxjs';
+import { AnimalService } from 'src/app/services/animal.service';
+import { TypeService } from 'src/app/services/type.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detail-animal',
   templateUrl: './detail-animal.component.html',
   styleUrls: ['./detail-animal.component.css']
 })
-export class DetailAnimalComponent implements OnInit {
+export class DetailAnimalComponent implements OnInit, OnDestroy {
+  idAnimal: number;
+  typeAnimal: string;
+  animal: Animal;
+  subscriptionAnimal: Subscription;
+  subscriptionType: Subscription;
 
-  constructor() { }
+  constructor(
+    private animalService: AnimalService,
+    private typeService: TypeService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.idAnimal = +this.route.snapshot.paramMap.get('id');
+
+    this.subscriptionAnimal = this.animalService
+      .getAnimalById(this.idAnimal)
+      .subscribe(
+        animal => {
+          this.animal = animal;
+          this.getType();
+        },
+        error => {
+          console.log('Error getting all animal by type');
+        }
+      );
   }
 
+  getType() {
+    this.subscriptionType = this.typeService
+      .getTypeById(this.animal.type)
+      .subscribe(
+        type => {
+          this.typeAnimal = type.name;
+          console.log('Success getting type');
+        },
+        error => {
+          console.log('Error getting type');
+        }
+      );
+  }
+
+  ngOnDestroy() {
+    if (this.subscriptionAnimal !== undefined) {
+      this.subscriptionAnimal.unsubscribe();
+    }
+    if (this.subscriptionType !== undefined) {
+      this.subscriptionType.unsubscribe();
+    }
+  }
 }
